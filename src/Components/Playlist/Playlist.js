4445dfd3735507cec91094df8a457c323db57a1a
Alan Yao <alanyao.training@gmail.com>
2021-06-08 20:11:05 -0700

    Convert whitespace

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,15 +3,19 @@ import './Playlist.css';
 import TrackList from '../TrackList/TrackList';
 
 class Playlist extends React.Component {
-    render() {
-        return (
-            <div className="Playlist">
-                <input defaultValue={'New Playlist'}/>
-                <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
-                <button className="Playlist-save">SAVE TO SPOTIFY</button>
-            </div>
-        );
-    }
+  handleNameChange() {
+    
+  }
+
+  render() {
+    return (
+      <div className="Playlist">
+        <input defaultValue={'New Playlist'}/>
+        <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
+        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+      </div>
+    );
+  }
 }
 
 export default Playlist;
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -3,19 +3,19 @@ import './TrackList.css';
 import Track from '../Track/Track';
 
 class TrackList extends React.Component {
-    render() {
-        const tracks = this.props.tracks;
-        return (
-            <div className="TrackList">
-                {
-                    tracks.map(track => {
-                        return <Track track={track} key={track.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} 
-                        isRemoval={this.props.isRemoval} />
-                    })
-                }
-            </div>
-        );
-    }
+  render() {
+    const tracks = this.props.tracks;
+    return (
+      <div className="TrackList">
+        {
+          tracks.map(track => {
+            return <Track track={track} key={track.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} 
+              isRemoval={this.props.isRemoval} />
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default TrackList;
